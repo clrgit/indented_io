6e4cf4383df463abdb809dd590fc246635a700f4
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2021-03-31 20:56:01 +0200

    Fix 'superclass mismatch' problem for StringIO

--- lib/indented_io/stringio.rb
+++ lib/indented_io/stringio.rb
@@ -1,3 +1,4 @@
+require 'stringio' # Requiered to avoid 'superclass mismatch' errors in other modules
 require 'indented_io/indented_io_interface'
 
 # Includes the IndentedIOInterface that define the #indent method
